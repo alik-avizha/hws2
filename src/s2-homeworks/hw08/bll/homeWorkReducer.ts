032a9a70bd3acd48361be0d2fab0454ab704dec4
Alik_Avizha <aleksandravizha@icloud.com>
2023-04-18 14:46:55 +0300

    work with Homework №8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -30,7 +30,7 @@ export const homeWorkReducer = (state: UserType[], action: ActionType): UserType
             return newState
         }
         case 'check': {
-            return state.filter(el => el.age >= 18)
+            return state.filter(el => el.age >= action.payload)
         }
         default:
             return state
