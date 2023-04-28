59d9eb946aa96a638efd7b10a8e4f2cea48c8e7d
Alik_Avizha <aleksandravizha@icloud.com>
2023-04-28 12:13:38 +0300

    work with Homework №10 and change styles in other HomeWorks

--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -1,11 +1,14 @@
 const initState = {
     isLoading: false,
 }
+type InitStateType = {
+    isLoading: boolean
+}
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+export const loadingReducer = (state = initState, action: LoadingActionType): InitStateType => {
     switch (action.type) {
-        // пишет студент  // need to fix
-
+        case 'CHANGE_LOADING':
+            return {...state, isLoading: action.isLoading}
         default:
             return state
     }
